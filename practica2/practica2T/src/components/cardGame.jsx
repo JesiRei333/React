48106f36800a9b98b3c2eba12da4b9c36fc458cc
Jesica Reyes <miucomosmos333@gmail.com>
2024-03-17 03:22:19 -0600

    practica2 arreglo de imagen rota

--- practica2/practica2T/src/components/cardGame.jsx
+++ practica2/practica2T/src/components/cardGame.jsx
@@ -5,13 +5,14 @@ export default function CardGames(props) {
     return(
         <article className="max-w-[167px] transition w-auto   leading-5 ">
             <div className=" bg-[rgb(14_14_16)] h-full ">
-           <div className="max-w-[167px] min-h-[223px]  bg-purple-400">
+           <div className="max-w-[165px] min-h-[220px]  bg-purple-400">
             <img className= " max-w-[167px] max-h-[223px]  hover:translate-x-3 hover:translate-y-[-10px] " src={props.imgSrc} alt={props.imgAlt || <img src="./src/img/Sin título.png" alt="" />} /> 
             </div>
             <div className="grid grid-cols-[90%_10%]">
            
                 <h3 className="text-[14px] text-neutral-300 hover:text-purple-400 font-semibold">{props.title}</h3>
-                <button><img src="./src/img/mostrar-mas-boton-con-tres-puntos.png"/>
+                <button><img className="max-w-[14px] max-h-[14px]  " src="  https://cdn-icons-png.flaticon.com/512/225/225287.png "/>
+                
                 </button>
                 <p className="text-[13px] text-[rgb(109,116,130)]">{props.views}</p>
                 <br />
